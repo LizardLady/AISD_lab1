e8af4c712c92af22a051b9fce0386faa2de38f91
Лизавета Земскова <harmony.lizard@gmail.com>
2024-10-26 17:54:50 +0300

    .

---
+++ src/HeapSort.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class HeapSort<T extends Comparable<T>> implements SortAlgorithm<T> {
+    @Override
+    public void sort(List<T> list) {
+        int length = list.size();
+        for (int i = length / 2 - 1; i >= 0; --i) {
+            differentiation(list, length, i);
+        }
+
+        for (int i = length - 1; i >= 0; --i) {
+            T temp = list.get(0);
+            list.set(0, list.get(i));
+            list.set(i, temp);
+            differentiation(list, i, 0);
+        }
+    }
+
+    private void differentiation(List<T> list, int length, int root) {
+        int largest = root;
+        int left = 2 * root + 1;
+        int right = 2 * root + 2;
+
+        if (left < length && list.get(left).compareTo(list.get(largest)) > 0) {
+            largest = left;
+        }
+
+        if (right < length && list.get(right).compareTo(list.get(largest)) > 0) {
+            largest = right;
+        }
+
+        if (largest != root) {
+            T temp = list.get(root);
+            list.set(root, list.get(largest));
+            list.set(largest, temp);
+
+            differentiation(list, length, largest);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,11 @@ public class Main {
         List<Integer> list7 = new ArrayList<>(List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
         new ShellSort<Integer>().sort(list7);
 
+        List<Integer> list8 = new ArrayList<>(List.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
+        new HeapSort<Integer>().sort(list8);
+
         List<Integer> list3 = new ArrayList<>();
-        for (int i = 1000; i > 0; --i){
+        for (int i = 1000; i >= 0; --i){
             list3.add(i);
         }
         new InsertionSort<Integer>().sort(list3);
@@ -35,5 +38,6 @@ public class Main {
         System.out.println(list5);
         System.out.println(list6);
         System.out.println(list7);
+        System.out.println(list8);
     }
 }
